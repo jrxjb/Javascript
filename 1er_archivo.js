37505013e539cd3ee998055f2a3508556831a7a2
jrxjb <177959714+jrxjb@users.noreply.github.com>
2024-11-02 22:49:20 -0400

    console.log, operaciones, variables,decremento y aumento

---
+++ 1er_archivo.js
@@ -0,0 +1,49 @@
+console.log("hola")
+//comentario 
+
+/*
+comentario 
+de varias 
+lineas
+*/
+
+var miVarable = "freeCodeCamp";
+console.log(miVarable)
+
+// Operaciones
+
+var suma = 7+12;
+var resultadoNegtivo = 12 - 13 ;
+var resultadoFinal;
+var pruebadivisionIntYFloat = 5.2/2,
+
+resultadoFinal = resultadoNegtivo + suma;
+console.log(suma);
+console.log(resultadoNegtivo);
+console.log(resultadoFinal);
+console.log(pruebadivisionIntYFloat);
+
+// incremento 
+console.log("incremnto");
+var libros = 0;
+var libros2 = 0;
+
+libros = libros + 2;
+console.log("resultado de libros = " + libros );
+libros2 ++;   // se incrmente de 1 en 1 
+// otra formade implimir con variables
+console.log (`resultado de libros2 = ${libros2}`);
+libros2 = libros + 1;
+console.log("libros2 resultado 2 = " + libros2 );
+
+// Decrementar
+
+var decrementar_1 = 8;
+decrementar_1 = decrementar_1 - 1; 
+console.log("Decrementar =" + decrementar_1);
+decrementar_1 --;
+console.log("Decrementar con -- seria = " + decrementar_1);
+decrementar_1 -= 5;
+console.log("seria con variable-=5 entonces nos queda = " + decrementar_1);
+
+//50m
